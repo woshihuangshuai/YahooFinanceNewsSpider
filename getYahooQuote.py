e09edbc2d3f7ed08ec5fe62fd231bdb3876ee402
Shuai <woshihuangshuai@hotmail.com>
2016-10-11 14:28:44 +0800

    修复bug

--- getYahooQuote.py
+++ getYahooQuote.py
@@ -8,24 +8,37 @@
 数据构成：日期, 开盘时间, 收盘时间, 股价列表
 '''
 
-from pymongo import MongoClient
-from urllib.request import urlopen
+from pymongo                            import MongoClient
+from urllib.request                     import urlopen
+from YahooFinanceNewsSpider.companies   import S_P_500_companies
 import json
 import collections
-from YahooFinanceNewsSpider.companies import S_P_500_companies
 
 
 def ConnectMongoDB():
     client = MongoClient('localhost', 27017)
-    return client
+    db = client['Quote']
+    collection = db['YahooFianceQuote']
+    return collection
+
 
 def WritetoMongoDB(collection, doc):
-    if collection.find_one({'date':doc['date']}) != None:
+    query = {'$and':[
+            {'company'  :   doc['company']},
+            {'date'     :   doc['date']}
+            ]}
+
+    cursor = collection.find_one(query)
+    if cursor != None:
+        print (cursor['company'], '\t', cursor['date'], '\t', 'is existed.')
         return -1
+
     collection.insert_one(doc)
     return 1
 
-def GetQuote(url):
+
+def GetQuote(company, url):
+    company_name = company
     docs = []
     series = []
     date = []
@@ -52,6 +65,7 @@ def GetQuote(url):
     for quote in series:
         if quote['Timestamp'] > timestamp_max[index]:
             document = collections.OrderedDict()
+            document['company'] = company_name
             document['date']    = date[index]
             document['min']     = timestamp_min[index]
             document['max']     = timestamp_max[index]
@@ -62,7 +76,8 @@ def GetQuote(url):
         quote_list.append(quote)
 
     document = collections.OrderedDict()
-    document['date']    = date[index],
+    document['company'] = company_name
+    document['date']    = date[index]
     document['min']     = timestamp_min[index]
     document['max']     = timestamp_max[index]
     document['quote']   = quote_list
@@ -70,21 +85,19 @@ def GetQuote(url):
 
     return docs
 
-if __name__ == '__main__':
-    timerange = 20 #day
 
+if __name__ == '__main__':
+    collection = ConnectMongoDB()
+    timerange = 20 #day
     for company in S_P_500_companies:
-        client = ConnectMongoDB()
-        db = client['Quote']
-        collection = db[company]
         url = 'http://chartapi.finance.yahoo.com/instrument/1.0/%s/chartdata;type=quote;range=%dd/json' % (company, timerange)
-        docs = GetQuote(url)
+        docs = GetQuote(company, url)
         if docs != None:
             for doc in docs:
                 WritetoMongoDB(collection, doc)
         else:
             print('ERROR: Can\'t get quote of', company)
-        client.close()
+
 
 
         
